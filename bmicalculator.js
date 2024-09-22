44983263ec99226d6b3584cccbe992d7d719d7e2
anaghakarurkar <anaghakarurkar@gmail.com>
2024-09-22 13:42:02 +0100

    Modified bmi calculator logic to return more specific interpretation

--- bmicalculator.js
+++ bmicalculator.js
@@ -1,4 +1,20 @@
 function bmiCalculator(weightInKg, heightInMeters) {
-    var bmi =  weightInKg / Math.pow(heightInMeters , 2);
-    return Math.round(bmi);
+    var bmi = weightInKg / Math.pow(heightInMeters, 2);
+    var interpretation;
+    
+    bmi = Math.floor(bmi);
+    
+    if(bmi < 18.5){
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    }
+    else {
+        if(bmi >=18.6 && bmi <= 24.9) {
+           interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+        }
+        else {
+           interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+        }
+    }
+    
+   return interpretation;
 }
