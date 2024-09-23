5fdfff9b0d56e1057f99dad16064b27e363e2c7f
anaghakarurkar <anaghakarurkar@gmail.com>
2024-09-23 12:58:35 +0100

    Refactored code.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,11 +4,9 @@ for(let i = 1; i <= 100; i++) {
    }
     else if(i % 3 == 0) {
        console.log("Fizz");
-       continue;
    }
     else if( i % 5 == 0 ){
        console.log("Buzz");
-       continue;
    }
     else {
         console.log(i);
